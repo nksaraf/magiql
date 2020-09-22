e215fa0938a140f699d1baa4006dbe3ea65b4954
Nikhil Saraf <nsaraf98@gmail.com>
2020-09-22 00:22:06 -0700

    add native options

---
+++ src/native.ts
@@ -0,0 +1,34 @@
+// @ts-ignore
+import { unstable_batchedUpdates, AppState } from "react-native";
+import { setConsole, setFocusHandler } from "react-query";
+
+// @ts-ignore
+import { setBatch } from "./core/store/batchedUpdates";
+
+export * from "./core/client";
+export * from "./core/fetch";
+export * from "./core/store/cacheStore";
+export * from "./core/store/normalizedCacheStore";
+export * from "./hooks";
+export * from "./core/store/batchedUpdates";
+export * from "./core/graphql-tag";
+
+setConsole({
+  log: console.log,
+  warn: console.log,
+  error: (error) => {},
+});
+
+setBatch(unstable_batchedUpdates);
+
+setFocusHandler((handleFocus) => {
+  const handleAppStateChange = (appState) => {
+    if (appState === "active") {
+      handleFocus();
+    }
+  };
+
+  AppState.addEventListener("change", handleAppStateChange);
+
+  return () => AppState.removeEventListener("change", handleAppStateChange);
+});
