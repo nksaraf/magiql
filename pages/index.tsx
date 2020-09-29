80397c494fba850efa7f35b76fa6f2f7eb5d4a00
Nikhil Saraf <nsaraf98@gmail.com>
2020-09-29 13:01:15 +0000

    fix up repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,60 @@
-export { People as default } from "../components/People";
+import React from "react";
+import { graphql, useQuery } from "magiql";
+import { Actions, ActionButton, Header } from "../components/ActionButton";
+import { NavBar } from "../components/NavBar";
+import { Person_person, Person } from "../components/Person";
+
+export default function People() {
+  const { data, status } = useQuery(
+    graphql`
+      query PeopleQuery($after: String) {
+        allPeople(first: 10, after: $after) {
+          edges {
+            node {
+              id
+              ...Person_person
+            }
+            cursor
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+
+      ${Person_person}
+    `,
+    {
+      getFetchMore: (lastpage) => ({
+        after: lastpage.allPeople.pageInfo.hasNextPage
+          ? lastpage.allPeople.pageInfo.endCursor
+          : null,
+      }),
+    }
+  );
+
+  return (
+    <>
+      <NavBar />
+      <Header>useQuery</Header>
+      <main>
+        <code style={{ fontFamily: "Roboto Mono" }}>
+          <b>status:</b> {status}
+        </code>
+        <Actions>
+          <ActionButton onClick={() => {}}>Refresh</ActionButton>
+        </Actions>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ flex: 1 }}>
+            {data
+              ? data.allPeople?.edges?.map((edge) => (
+                  <Person key={edge.node.id} person={edge.node} />
+                ))
+              : null}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
--- pages/infinite.tsx
+++ pages/infinite.tsx
@@ -1,1 +1,89 @@
-export { PeopleInfinite as default } from "../components/PeopleInfinite";
+import React from "react";
+import { useInfiniteQuery, graphql } from "magiql";
+import { Person, Person_person } from "../components/Person";
+import { NavBar } from "../components/NavBar";
+import { Header, Actions, ActionButton } from "../components/ActionButton";
+
+export default function PeopleInfinite() {
+  const { data, fetchMore, status, isFetchingMore } = useInfiniteQuery(
+    graphql`
+      query PeopleInfiniteQuery(
+        $limit: Int = 10
+        $after: String = "YXJyYXljb25uZWN0aW9uOjk="
+      ) {
+        allPeople(first: $limit, after: $after) {
+          edges {
+            node {
+              id
+              name
+              homeworld {
+                id
+                name
+              }
+              ...Person_person
+            }
+            cursor
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+        allFilms {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+
+      ${Person_person}
+    `,
+    {
+      variables: {
+        limit: 10,
+      },
+      getFetchMore: (lastpage) => ({
+        after: lastpage.allPeople.pageInfo.hasNextPage
+          ? lastpage.allPeople.pageInfo.endCursor
+          : null,
+      }),
+    }
+  );
+
+  return (
+    <>
+      <NavBar />
+      <Header>useInfiniteQuery</Header>
+      <main>
+        <code style={{ fontFamily: "Roboto Mono" }}>
+          <b>status:</b> {status}
+        </code>
+        <Actions>
+          <ActionButton
+            onClick={() => {
+              fetchMore();
+            }}
+          >
+            Fetch more
+          </ActionButton>
+        </Actions>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ flex: 1 }}>
+            {data?.map((page, index) => (
+              <React.Fragment key={index}>
+                {page
+                  ? page.allPeople?.edges?.map((edge) => (
+                      <Person key={edge.node.id} person={edge.node} />
+                    ))
+                  : null}
+              </React.Fragment>
+            ))}
+            {isFetchingMore && "Fetching more..."}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
