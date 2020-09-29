600b68fd83cdc2c496aff21d2ae3559012dc093e
Nikhil Saraf <nsaraf98@gmail.com>
2020-09-29 02:44:40 -0700

    update reader

--- src/core/reader copy.tsx
+++ src/core/reader copy.tsx
@@ -16,7 +16,7 @@ import {
   Record,
 } from "./types";
 
-const fieldReader = (get) => ({
+const fieldReader = (get, subscribe) => ({
   readField: (field: ReaderField, variables, dataID) => {
     const fieldName = field.alias ?? field.name;
     const storageKey = getStorageKey(field, variables);
@@ -37,7 +37,12 @@ const recordReader = (get, subscribe) => ({
   readRecord: (node, dataID) => {
     return get(dataID);
   },
-  subscribe,
+  subscribeToRecord: (dataID) => {
+    subscribe(dataID);
+  },
+  subscribeToField: (dataID) => {
+    subscribe(dataID);
+  },
 });
 
 function hasData(data) {
@@ -195,15 +200,9 @@ export const readFragmentAsRecords = (selector, get) => {
   return { data: snaphot, seenRecords: [...dataReader.seenRecords.values()] };
 };
 
-export function readFragmentAsFields<TData>(
-  selector: SingularReaderSelector
-) {
+export function readFragmentAsFields<TData>(selector: SingularReaderSelector) {
   const reader = fieldReader(getter);
-  function traverse(node: ReaderNode, dataID: string, prevData: any) {
-    const data = prevData || {};
-    traverseSelections(node.selections, dataID, data);
-    return data;
-  }
+
   function readScalar(field: ReaderScalarField, dataID: string, data: any) {
     const { fieldName, value } = reader.readField(
       field,
@@ -314,5 +313,121 @@ export function readFragmentAsFields<TData>(
     }
   }
   const { node, dataID } = selector;
+  function traverse(node: ReaderNode, dataID: string, prevData: any) {
+    const data = prevData || {};
+    traverseSelections(node.selections, dataID, data);
+    return data;
+  }
+
   return traverse(node, dataID, null) as TData;
+}
+
+interface Reader<TRecord> {
+  readField: (
+    field: ReaderField,
+    variables: any,
+    record: TRecord
+  ) => { fieldName: string; value: any };
+  readRecord: (node: ReaderNode, dataID: string) => TRecord;
+  subscribeToRecord: (dataID: string) => void;
+  subscribeToField: () => {};
+}
+
+function readFragment<TData, TRecord extends string>(
+  reader: Reader<TRecord>,
+  selector
+) {
+  function createFragmentPointer(
+    fragmentSpread: ReaderFragmentSpread,
+    record: TRecord,
+    data: SelectorData
+  ): void {
+    let fragmentPointers: any = data[constants.FRAGMENTS_KEY];
+    if (fragmentPointers == null) {
+      fragmentPointers = data[constants.FRAGMENTS_KEY] = {};
+    }
+    if (data[constants.ID_KEY] == null) {
+      data[constants.ID_KEY] = dataID;
+    }
+    fragmentPointers[fragmentSpread.name] = {};
+    data[constants.FRAGMENT_OWNER_KEY] = selector.owner;
+  }
+
+  function traverseSelections(
+    selections: readonly ReaderSelection[],
+    record: TRecord,
+    data: SelectorData
+  ) {
+    for (let i = 0; i < selections.length; i++) {
+      const selection = selections[i];
+      switch (selection.kind) {
+        case RelayConcreteNode.SCALAR_FIELD: {
+          const field = selection as ReaderScalarField;
+          const { fieldName, value } = reader.readField(
+            field,
+            selector.variables,
+            record
+          );
+          data[fieldName] = value;
+          break;
+        }
+        case RelayConcreteNode.LINKED_FIELD: {
+          const field = selection as ReaderLinkedField;
+
+          const { fieldName, value } = reader.readField(
+            field,
+            selector.variables,
+            record
+          );
+
+          if (field.plural || isRefs(value)) {
+            // readPluralLink(selection as ReaderLinkedField, record, data);
+          } else {
+            if (value == null) {
+              data[fieldName] = null;
+              return null;
+            }
+
+            data[fieldName] = traverse(
+              field,
+              value[constants.REF_KEY],
+              data[fieldName]
+            );
+          }
+        }
+        case RelayConcreteNode.FRAGMENT_SPREAD:
+          createFragmentPointer(
+            selection as ReaderFragmentSpread,
+            record,
+            data
+          );
+          break;
+      }
+    }
+  }
+
+  function traverse(node: ReaderNode, dataID: string, prevData: any) {
+    const record = reader.readRecord(node, dataID);
+    if (record === null) {
+      return null;
+    }
+
+    const data = prevData || {};
+    traverseSelections(node.selections, record, data);
+    if (hasData(data)) {
+      reader.subscribeToRecord(dataID);
+    }
+    return data;
+  }
+
+  const { node, dataID } = selector;
+  return traverse(node, dataID, null) as TData;
+}
+
+function isRefs(value) {
+  return typeof value === "object" && Boolean(value[constants.REFS_KEY]);
+}
+
+function isRef(value) {
+  return typeof value === "object" && Boolean(value[constants.REF_KEY]);
 }
